9948e97e883159106f6c368026602776d1a10035
diogoesteves <diogo.esteves@eino.ai>
2023-11-12 13:59:50 +0000

    feat(e2e): app.cy.ts
    Introduces initial e2e tests

--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -13,12 +13,31 @@ describe('e2e', () => {
     cy.get('[data-cy="create-board-button-empty-state"]').click();
     cy.get('input').type('Board 1');
     cy.get('[data-cy="create-board-modal-create-button"]').click();
-  });
 
-  it('should create a new board from header button', () => {
-    cy.visit('/');
-    cy.get('button').contains('Create New Board').click();
-    cy.get('input').type('Board 2');
-    cy.get('[data-cy="create-board-modal-create-button"]').click();
+    cy.get('button').contains('Create New Card').click();
+    cy.get('[data-cy="card-title"').first().type('Card 1');
+    cy.get('textarea').type('Card 1 description');
+    cy.get('[data-cy="create-card-modal-create-button"]').click();
+
+    cy.get('button').contains('Create New Card').click();
+    cy.get('[data-cy="card-title"').first().type('Card 2');
+    cy.get('textarea').type('Card 2 description');
+    cy.get('[data-cy="create-card-modal-create-button"]').click();
+
+    const dataTransfer = new DataTransfer();
+
+    cy.get('[data-cy="card-to-drag"]')
+      .first()
+      .trigger('dragstart', { dataTransfer });
+    cy.get('[data-cy="column drop zone"]')
+      .last()
+      .trigger('drop', { dataTransfer });
+
+    cy.get('[data-cy="card-to-drag"]')
+      .first()
+      .trigger('dragstart', { dataTransfer });
+    cy.get('[data-cy="column drop zone"]')
+      .last()
+      .trigger('drop', { dataTransfer });
   });
 });
